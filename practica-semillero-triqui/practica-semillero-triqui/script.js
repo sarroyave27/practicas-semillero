7e8b285f9eb392d0155fe1791963a00e075d182c
sarroyave27 <69723690+sarroyave27@users.noreply.github.com>
2022-10-17 23:34:45 -0500

    Avances-restablecer

---
+++ practica-semillero-triqui/practica-semillero-triqui/script.js
@@ -0,0 +1,194 @@
+const btn = document.querySelector('#btn-game');
+const start = document.querySelector('#text-partidas');
+const time1 = document.querySelector('reloj')
+const rPlayer = document.querySelector('player1')
+let turno = 1;
+
+
+let tiempoRef = Date.now()
+let cronometrar = false;
+let acumulado = 0
+
+// let tiempoRef2 = Date.now()
+// let cronometrar2 = false
+// let acumulado2 = 0
+
+function iniciar() {
+    cronometrar = true
+}
+
+function pausar() {
+    cronometrar = false
+}
+
+function reiniciar() {
+    acumulado = 0
+}
+
+// function iniciar2() {
+//     cronometrar2 = true
+// }
+
+// function pausar2() {
+//     cronometrar2 = false
+// }
+
+// function reiniciar2() {
+//     acumulado2 = 0
+// }
+
+// btn.addEventListener('click', () => {
+//     if(cronometrar === false) {
+//             setInterval(() => {
+//                 let tiempo = document.getElementById("tiempo")
+//                 if (cronometrar) {
+//                     acumulado += Date.now() - tiempoRef
+//                 }
+//                 tiempoRef = Date.now()
+//                 tiempo.innerHTML = formatearMS(acumulado)
+//             }, 1000 / 60);
+//         }
+// })
+btn.addEventListener('click', () => {
+    if (start.value > 0) {
+        if (start.value % 2 == 0) {
+            alert('Ingrese un numero impar');
+            error();
+        } else {
+            // textStart.readOnly = true;
+            // if(rPlayer.checked) {
+            //     cronometro(time1);
+            // }else {
+            //     cronometro(time2);
+            // }
+            setInterval(() => {
+                let tiempo = document.getElementById("tiempo")
+                if (cronometrar) {
+                    acumulado += Date.now() - tiempoRef
+                }
+                tiempoRef = Date.now()
+                tiempo.innerHTML = formatearMS(acumulado)
+            }, 1000 / 60);
+            
+            // setInterval(() => {
+            //     let tiempo2 = document.getElementById("tiempo2")
+            //     if (cronometrar2) {
+            //         acumulado2 += Date.now() - tiempoRef2
+            //     }
+            //     tiempoRef2 = Date.now()
+            //     tiempo2.innerHTML = formatearMS(acumulado2)
+            // }, 1000 / 60);
+        }
+    } else {
+        error();
+        console.log("Debe ingresar numero de partidas");
+    }
+})
+
+const error = () => {
+    start.style.borderColor = "red";
+
+}
+
+
+function formatearMS(tiempo_ms) {
+    let MS = tiempo_ms % 1000
+    
+    let St = Math.floor(((tiempo_ms - MS) / 1000))
+    
+    let S = St%60
+    let M = Math.floor((St / 60) % 60)
+    let H = Math.floor((St/60 / 60))
+    Number.prototype.ceros = function (n) {
+        return (this + "").padStart(n, 0)
+    }
+
+    return H.ceros(2) + ":" + M.ceros(2) + ":" + S.ceros(2)
+        + "." + MS.ceros(3)
+}
+function cronometro(timer){
+        time1.innerHTML = 0;
+        t = setInterval(()=>{
+            time1.innerHTML = parseInt(time1.innerHTML) + 1
+            if (timer.innerHTML == 60){
+                setTimeout(()=>{
+                    window.alert("Se ha acabado el tiempo");
+                    document.getElementById()
+                })
+            }
+        },1000)
+}
+
+const clickear = document.querySelector(".game");
+function evaluar(elemento) {
+    //   HORIZONTALES  
+    if (clickear.children[4].children[0].innerHTML == elemento &&
+        clickear.children[4].children[1].innerHTML == elemento &&
+        clickear.children[4].children[2].innerHTML == elemento) {
+        console.log("Ha ganado: " + elemento)             
+    }
+    if (clickear.children[5].children[0].innerHTML == elemento &&
+        clickear.children[5].children[1].innerHTML == elemento &&
+        clickear.children[5].children[2].innerHTML == elemento) {
+        console.log("Ha ganado: " + elemento)
+    }
+    if (clickear.children[6].children[0].innerHTML == elemento &&
+        clickear.children[6].children[1].innerHTML == elemento &&
+        clickear.children[6].children[2].innerHTML == elemento) {
+        alert("Ha ganado: " + elemento)
+        }
+    //  VERTICLES    
+    if (clickear.children[4].children[0].innerHTML == elemento &&
+        clickear.children[5].children[0].innerHTML == elemento &&
+        clickear.children[6].children[0].innerHTML == elemento) {
+        alert("Ha ganado: " + elemento)
+
+        }
+    if (clickear.children[4].children[1].innerHTML == elemento &&
+        clickear.children[5].children[1].innerHTML == elemento &&
+        clickear.children[6].children[1].innerHTML == elemento) {
+        alert("Ha ganado: " + elemento)
+
+        }
+    if (clickear.children[4].children[2].innerHTML == elemento &&
+        clickear.children[5].children[2].innerHTML == elemento &&
+        clickear.children[6].children[2].innerHTML == elemento) {
+        alert("Ha ganado: " + elemento)
+        }
+    //  DIAGONALES 
+    if (clickear.children[4].children[0].innerHTML == elemento &&
+        clickear.children[5].children[1].innerHTML == elemento &&
+        clickear.children[6].children[2].innerHTML == elemento) {
+        alert("Ha ganado: " + elemento)
+        }
+    if (clickear.children[4].children[2].innerHTML == elemento &&
+        clickear.children[5].children[1].innerHTML == elemento &&
+        clickear.children[6].children[0].innerHTML == elemento) {
+        alert("Ha ganado: " + elemento)
+        }
+
+    if (alert === true){
+        elemento.readOnly = true;
+    }
+}
+
+
+function marcar(obj) {
+    if (turno == 1) {
+        if (obj.target.innerHTML === "") {
+            obj.target.innerHTML = "X";
+            turno = 2;
+        }
+    } else {
+        if (obj.target.innerHTML === "") {
+            obj.target.innerHTML = "O";
+            turno = 1;
+        }
+    }
+    evaluar(obj.target.innerHTML);
+}
+
+function handleRestartGame() {
+    document.querySelectorAll('.game-cell').forEach(cell => cell.innerHTML = "")
+  }
+
